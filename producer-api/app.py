b6346aa4d3448ecb2c8e4f14fdbbb90913c6c767
mustafalatif354 <mustafa.latif354@gmail.com>
2026-03-11 10:23:19 +0100

    fixed producer server

--- producer-api/app.py
+++ producer-api/app.py
@@ -5,7 +5,7 @@ import json
 app = FastAPI()
 
 producer = KafkaProducer(
-    bootstrap_servers = "kafka:9092",
+    bootstrap_servers = "localhost:9092",
     value_serializer = lambda v: json.dumps(v).encode("utf-8")
 )
 
