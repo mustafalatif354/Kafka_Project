b50b97fcae58ede4f8559b558ac1be31f47d3317
mustafalatif354 <mustafa.latif354@gmail.com>
2026-03-11 10:05:30 +0100

    first commit

---
+++ producer-api/app.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from kafka import KafkaProducer
+import json
+
+app = FastAPI()
+
+producer = KafkaProducer(
+    bootstrap_servers = "kafka:9092",
+    value_serializer = lambda v: json.dumps(v).encode("utf-8")
+)
+
+@app.post("/orders")
+def create_order(order_id: int, amount: int):
+
+    event = {
+        "order_id": order_id,
+        "amount": amount
+    }
+
+    producer.send("orders", event)
+
+    return {"status": "sent"}
